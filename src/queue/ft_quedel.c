9f6d728d9e2d805d69480c94fd3be84b87289ba8
Beach Linnea <blinnea@oa-j5.msk.21-school.ru>
2020-07-25 15:58:56 +0300

    ft_quedel now cn remove queue witjout content

--- src/queue/ft_quedel.c
+++ src/queue/ft_quedel.c
@@ -6,7 +6,7 @@
 /*   By: blinnea <blinnea@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/01 18:16:42 by blinnea           #+#    #+#             */
-/*   Updated: 2020/07/01 22:14:46 by blinnea          ###   ########.fr       */
+/*   Updated: 2020/07/23 12:43:56 by blinnea          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,8 @@ void	ft_quedel(t_queue **queue, void (*del)(void *, size_t))
 	if (!ft_queisempty(*queue))
 	{
 		(*queue)->tail = NULL;
-		ft_lstdel(&((*queue)->head), del);
+		(del ? ft_lstdel(&((*queue)->head), del) :
+		ft_lstdel_ic(&((*queue)->head)));
 	}
 	ft_memdel((void **)queue);
 }
